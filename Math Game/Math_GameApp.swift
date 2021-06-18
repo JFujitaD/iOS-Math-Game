1e1ffff9742308c064137b96043b1be4976b1146
JFujitaD <fujitajulian@icloud.com>
2021-06-18 15:20:30 -0600

    Initial Commit

---
+++ Math Game/Math_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Math_GameApp.swift
+//  Math Game
+//
+//  Created by Julian Dean on 6/18/21.
+//
+
+import SwiftUI
+
+@main
+struct Math_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

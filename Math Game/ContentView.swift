beb0003bbe935202b097cd017fa57b8a1211a91d
JFujitaD <fujitajulian@icloud.com>
2021-06-18 15:43:26 -0600

    Home screen finished

--- Math Game/ContentView.swift
+++ Math Game/ContentView.swift
@@ -9,8 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            NavigationView {
+                VStack {
+                    Text("+ - Choose A Game / *").font(.title)
+                    Spacer()
+                    Group{
+                        Group{
+                            Divider()
+                            NavigationLink(destination: Text("Test")) {
+                                Text("Addition")
+                            }
+                            Divider()
+                            NavigationLink(destination: Text("Test")) {
+                                Text("Subtraction")
+                            }
+                            Divider()
+                            NavigationLink(destination: Text("Test")) {
+                                Text("Multiplication")
+                            }
+                            Divider()
+                            NavigationLink(destination: Text("Test")) {
+                                Text("Division")
+                            }
+                        }
+                        Group{
+                            Divider()
+                            NavigationLink(destination: Text("Test")) {
+                                Text("High Scores").foregroundColor(.green)
+                            }
+                            Divider()
+                        }
+                    }
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(.red)
+                    Spacer()
+                }.navigationTitle("Math Game Home")
+            }
+        }
     }
 }
 

7ab4a0ba4bec982555817bd4fedba929a8e40c43
JFujitaD <fujitajulian@icloud.com>
2021-06-18 19:37:55 -0600

    Implemented score mechanism

--- Math Game/Views/GameView.swift
+++ Math Game/Views/GameView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct GameView: View {
     let game: Game
     
+    @State var score: Int = 0
     @State var answer: Int = 0
     @State var answerField = "0"
     @State var num1 = "_"
@@ -21,7 +22,7 @@ struct GameView: View {
             Text(game.name).font(.largeTitle)
             Spacer()
             Divider()
-            Text("Solve the equation!").font(.title)
+            Text("Score: \(score)").font(.title)
             HStack{
                 Group{
                     Spacer()
@@ -34,14 +35,7 @@ struct GameView: View {
                     Text("=")
                     Spacer()
                 }.font(.title)
-                Group{
-                    TextField("", text: $answerField)
-                        .frame(width: 60.0)
-                        .keyboardType(/*@START_MENU_TOKEN@*/.numberPad/*@END_MENU_TOKEN@*/)
-                        .multilineTextAlignment(/*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                        .font(.title)
-                    Spacer()
-                }
+                AnswerView(game: game, num1: $num1, num2: $num2, answerField: $answerField, answer: $answer, score: $score)
             }
             .padding(.horizontal)
             Spacer()
@@ -51,11 +45,40 @@ struct GameView: View {
                 num1 = String(int1)
                 num2 = String(int2)
                 answer = game.computeAnswer(n1: int1, n2: int2)
-                print(answer)
             }, label: {
                 Text("Start Game")
             }).disabled(buttonDisabled)
             Divider()
+            Spacer()
+        }
+    }
+}
+
+struct AnswerView: View {
+    let game: Game
+    @Binding var num1: String
+    @Binding var num2: String
+    @Binding var answerField: String
+    @Binding var answer: Int
+    @Binding var score: Int
+    
+    var body: some View {
+        Group{
+            TextField("", text: $answerField)
+                .frame(width: 60.0)
+                .keyboardType(/*@START_MENU_TOKEN@*/.numberPad/*@END_MENU_TOKEN@*/)
+                .multilineTextAlignment(/*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .font(.title)
+                .onChange(of: answerField, perform: { value in
+                    if Int(answerField) == answer {
+                        score += 1
+                        let (int1, int2) = game.getRandomNumbers()
+                        num1 = String(int1)
+                        num2 = String(int2)
+                        answer = game.computeAnswer(n1: int1, n2: int2)
+                        answerField = ""
+                    }
+                })
             Spacer()
         }
     }
